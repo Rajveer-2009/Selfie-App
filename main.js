e8b9d041e58b96469d7e72ab3f5f984e3a867760
Rajveer-2009 <84115638+Rajveer-2009@users.noreply.github.com>
2022-06-08 15:28:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,23 +7,38 @@ function start() {
 }
 
 recognition.onresult = function (event) {
-
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
     console.log(content);
-    speak()
-
+    if(content == "take my selfie") {
+        speak()
+    }
 }
 
 function speak() {
-
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking Your Selfie in 5 Seconds"
     var utterthis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterthis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    }, 5000);
+}
 
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click()
 }
 
 Webcam.set({
